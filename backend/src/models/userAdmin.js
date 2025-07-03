b7dc60cd593cbe29d4b957179920f884ccc49ccd
Yowanehaki <hazwiartaf@gmail.com>
2025-07-03 11:30:35 +0700

    del admin def

--- backend/src/models/userAdmin.js
+++ backend/src/models/userAdmin.js
@@ -17,23 +17,6 @@ const getAdmin = async () => {
   return prisma.admin.findMany();
 };
 
-// Fungsi untuk membuat admin default jika belum ada
-const createDefaultAdmin = async () => {
-  const admin = await prisma.admin.findFirst({ where: { username: 'admin' } });
-  if (!admin) {
-    const hashed = await bcrypt.hash('admin123', 10);
-    await prisma.admin.create({
-      data: {
-        username: 'admin',
-        password: hashed,
-      },
-    });
-    console.log('Default admin created: admin/admin123');
-  } else {
-    console.log('Default admin already exists');
-  }
-};
-
 module.exports = {
   createAdmin,
   getAdmin,
